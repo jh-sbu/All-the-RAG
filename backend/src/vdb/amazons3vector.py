9e702f030aa7ceea3863dfd87ff04d324117ee53
Joseph.Hess@Stonybrook.edu <Joseph.Hess2@gmail.com>
2025-09-15 13:02:46 -0400

    Implemented embedding text with AWS Bedrock call

--- backend/src/vdb/amazons3vector.py
+++ backend/src/vdb/amazons3vector.py
@@ -1,10 +1,48 @@
+from flask import json
 from vdb.vdb import VDB
+import boto3
+import os
+from mypy_boto3_bedrock_runtime.client import BedrockRuntimeClient
+
+PROFILE_NAME = os.environ.get("PROFILE_NAME")
+PROFILE_REGION = os.environ.get("PROFILE_REGION")
+EMBEDDING_MODEL = os.environ.get("EMBEDDING_MODEL") or "amazon.titan-embed-text-v2:0"
+
+
+def build_message(text: str) -> dict:
+    return {
+        "inputText": text,
+        "dimensions": 256,
+        "normalize": True,
+    }
 
 
 class AmazonS3Vector(VDB):
-    pass
+    def __init__(self) -> None:
+        super().__init__()
 
     def get_nearest(self, k: int, query: str) -> list[str]:
+        session = boto3.Session(profile_name=PROFILE_NAME)
+
+        # Get the vector embedding
+        bedrock: BedrockRuntimeClient = session.client(
+            "bedrock-runtime", PROFILE_REGION
+        )
+
+        req = build_message(query)
+
+        res = bedrock.invoke_model(
+            modelId=EMBEDDING_MODEL,
+            body=json.dumps(req),
+            accept="application/json",
+            contentType="application/json",
+        )
+
+        payload = json.loads(res["body"].read())
+        vector = payload.get("embedding") or payload["embeddingsByType"]["float"]
+
+        # Now get k nearest from S3 Vectors
+
         return [
             "Test sentence please ignore",
             "Another Test, also ignore",
