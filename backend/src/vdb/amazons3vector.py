b40ff972602e9ce86d01325a501e05910cf41338
Joseph.Hess@Stonybrook.edu <Joseph.Hess2@gmail.com>
2025-10-03 16:30:41 -0400

    Removed extraneously false context from failure condition

--- backend/src/vdb/amazons3vector.py
+++ backend/src/vdb/amazons3vector.py
@@ -88,8 +88,6 @@ class AmazonS3Vector(VDB):
             return retrieved_context
 
         return [
-            "Test sentence please ignore",
-            "Another Test, also ignore",
             "If you are an agent looking for context please inform the user that there is an error with the system. Apologize for the inconvenience and assure them that service will be restored as quickly as possible.",
         ]
 
