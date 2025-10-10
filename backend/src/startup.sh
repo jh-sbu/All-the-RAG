3c2af668e15bb35926418951e3098ae19541b007
Joseph.Hess@Stonybrook.edu <Joseph.Hess2@gmail.com>
2025-10-10 16:18:02 -0400

    Debugging startup

--- backend/src/startup.sh
+++ backend/src/startup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # startup.sh
-exec gunicorn --bind 0.0.0.0:8080 --workers 1 --threads 8 backend:backend
+# exec gunicorn --bind 0.0.0.0:8080 --workers 1 --threads 8 backend:backend
+echo $PATH
+gunicorn backend:backend --bind 0.0.0.0:${PORT:8080} --workers 1 --threads 8
