551d0daa8ec497339318def8a029fd663105b109
Joseph.Hess@Stonybrook.edu <Joseph.Hess2@gmail.com>
2025-10-12 16:06:41 -0400

    Startup tweaks

--- backend/src/startup.sh
+++ backend/src/startup.sh
@@ -2,4 +2,8 @@
 # startup.sh
 # exec gunicorn --bind 0.0.0.0:8080 --workers 1 --threads 8 backend:backend
 echo $PATH
-gunicorn backend:backend --bind 0.0.0.0:${PORT:8080} --workers 1 --threads 8
+echo $LAMBDA_TASK_ROOT
+# ls $LAMBDA_TASK_ROOT
+# PATH=$PATH:$LAMBDA_TASK_ROOT/bin PYTHONPATH=$LAMBDA_TASK_ROOT which gunicorn
+# PATH=$PATH:$LAMBDA_TASK_ROOT/bin PYTHONPATH=$LAMBDA_TASK_ROOT which python
+PATH=$PATH:$LAMBDA_TASK_ROOT/bin PYTHONPATH=$LAMBDA_TASK_ROOT exec python -m gunicorn backend:backend --bind 0.0.0.0:${PORT:8080} --workers 1 --threads 8
