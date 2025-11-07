cf8678126745619541da1e29e8fcf15e51ee99d3
Joseph.Hess@Stonybrook.edu <Joseph.Hess2@gmail.com>
2025-11-07 11:17:57 -0500

    Created uuid type and field

--- frontend/src/Models/ChatSession.tsx
+++ frontend/src/Models/ChatSession.tsx
@@ -1,7 +1,13 @@
 import { IMessage } from "./Message"
 
+// type UUID = `${string}-${string}-${string}-${string}-${string}`;
+// 7c0c48ff-d7c8-4b5a-8f3b-97fa3c13a497
+
+declare const __uuidBrand: unique symbol;
+export type UUID = string & { readonly [__uuidBrand]: 'UUID' };
 
 export interface IChatSession {
+  id: UUID | "None";
   chat_title: string,
   messages: IMessage[]
 }
